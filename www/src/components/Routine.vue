490e3a89525fe26fffeaf3b8754d90d93b27642a
Kyle Hyer <krhyer@gmail.com>
2017-10-17 10:37:59 -0600

    added v-shows

--- www/src/components/Routine.vue
+++ www/src/components/Routine.vue
@@ -1,36 +1,40 @@
 <template>
   <div class="routine">
-    <h4>Select your dance:</h4>
-    <div class="row">
-      <div class="col s6 m3">
-        <h5>Waltz</h5>
-      </div>
-      <div class="col s6 m3">
-        <h5>Tango</h5>
-      </div>
-      <div class="col s6 m3">
-        <h5>Foxtrot</h5>
-      </div>
-      <div class="col s6 m3">
-        <h5>Quickstep</h5>
+    <div v-show="showDance">
+      <h4>Select your dance:</h4>
+      <div class="row">
+        <div class="col s6 m3">
+          <h5>Waltz</h5>
+        </div>
+        <div class="col s6 m3">
+          <h5>Tango</h5>
+        </div>
+        <div class="col s6 m3">
+          <h5>Foxtrot</h5>
+        </div>
+        <div class="col s6 m3">
+          <h5>Quickstep</h5>
+        </div>
       </div>
     </div>
-    <h4>Select your level:</h4>
-    <div class="row">
-      <div class="col s12 m4">
-        <h5>Bronze</h5>
-      </div>
-      <div class="col s12 m4">
-        <h5>Silver</h5>
-      </div>
-      <div class="col s12 m4">
-        <h5>Gold</h5>
+    <div v-show="showLevel">
+      <h4>Select your level:</h4>
+      <div class="row">
+        <div class="col s12 m4">
+          <h5>Bronze</h5>
+        </div>
+        <div class="col s12 m4">
+          <h5>Silver</h5>
+        </div>
+        <div class="col s12 m4">
+          <h5>Gold</h5>
+        </div>
       </div>
     </div>
     <div class="col-md-4" v-for="step in steps">
       <router-link:to="'/steps/'+step._id">
         <p>{{step.name}}</p>
-      </router-link>
+        </router-link>
     </div>
   </div>
 </template>
@@ -40,10 +44,21 @@
   export default {
     name: 'routine',
     data() {
-      return {}
+      return {
+        showDance: true,
+        showLevel: false
+      }
     },
     computed: {},
-    methods: {},
+    methods: {
+      closeDance(){
+        this.showDance = !this.showDance,
+        this.showLevel = !this.showLevel
+      },
+      closeLevel(){
+        this.showLevel = this.showLevel
+      }
+    },
     components: {}
   }
 
